54898eb68cddbcfa99aca634ab23eadeea7a64d7
Emery2100 <Emery2100@yahoo.com>
2025-06-16 19:26:52 -0700

    variables

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("script");
+//VARIABLES
+
+//Strings
+let firstName = "David";
+let lastName = "Emery";
+
+//numerical
+let age = 99;
+let mySize = 10;
+
+//boolean values
+letisStudent = true;
+
+console.log(firstName);
+console.log(age,mySize);
+
+//concatenation
+console.log("My name is " + firstName + " " + lastName);
+
+//Operations
+
+let num1 = 10;
+let num2 = 2;
+
+let addition = num1 + num2;
+let subs = num1 - num2;
+let div = num1 / num2;
+let mult= num1 * num2;
+
+console.log("Addition is: " + addition);
+console.log("Subs is " + subs);
+console.log("Div is " + div);
+console.log("Mult is " + mult);
+
+let a;
+const pi = 3.1416
+let r = 3;
+
+a = pi * (r*r);
+console.log("The area of a circle with a radius of " + r + " is the " + a.toFixed(2));
+
+let aVar = 20;
+let otherVar = 30;
+
+console.log(aVar,otherVar);
+
+aVar = 50;
+otherVar = 60;
+
+console.log(aVar,otherVar);
+
+const weekDays = 5;
+
+//weekDays = 8; // the constant can't change
+
+console.log(weekDays);
