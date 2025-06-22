3b543caff6626e1113251f0631ff77ad8c7c4c20
Emery2100 <Emery2100@yahoo.com>
2025-06-22 12:55:51 -0700

    Assignment 3

---
+++ scripts/assignment3.js
@@ -0,0 +1,26 @@
+ function convertTemperature() {
+   
+            const temp = parseFloat(document.getElementById("temperature").value);
+            const scale = document.getElementById("scale").value;
+            
+            let convertedTemp;
+            let resultText;
+
+            if (scale === "C") {
+                convertedTemp = (temp * 9/5) + 32;
+                resultText = `${temp} Celsius is equal to ${convertedTemp.toFixed(2)} Fahrenheit.`;
+            } else if (scale === "F") {
+                convertedTemp = (temp - 32) * 5/9;
+                resultText = `${temp} Fahrenheit is equal to ${convertedTemp.toFixed(2)} Celsius.`;
+            }
+
+            const resultElement = document.getElementById("result");
+            resultElement.textContent = resultText;
+
+            if (convertedTemp <= 0) {
+                resultElement.style.color = "blue"; 
+            } else if (convertedTemp >= 32) {
+                resultElement.style.color = "red"; 
+            }
+        }
+                 
